34978282399e8e331726f086bd754555680b5cf0
Luisa Barbalho <luisa.barbalhops@gmail.com>
2020-10-14 10:27:13 -0300

    list orphanage on map with backend api

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
@@ -7,8 +7,35 @@ import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
 import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
+
+interface Orphanage{
+    id: number,
+    latitude: number,
+    longitude: number,
+    name: string
+}
 
 function OrphanagesMap() {
+
+    // State - Alterada pelo próprio componente
+    // setNomeDoState -> função para atualizar a lista
+
+    //<Orphanage[]> -> um array de formato Orphanage
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+    console.log(orphanages);
+    // Carregar da api do back assim que o componente é carregado
+    // primeiro parametro: QUAL ação deve ser executada
+    // segundo parametro: QUANDO as informações dentro do array tiverem seus valores alterados
+
+    //se usar axios.get -> vai chamar na api toda vez que for renderizado o componente - MUITAS chamadas -> melhor usar user Effect
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data);
+        })
+    }, []);
+
     return(
         <div id="page-map">
             <aside>
@@ -35,18 +62,23 @@ function OrphanagesMap() {
                     url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
 
-                <Marker
-                    icon = {mapIcon}
-                    position={[-5.8105031,-35.2442199]}
-                >
-                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-                        Lar das menindas
-                        <Link to="/orphanages/1">
-                            <FiArrowRight size={20} color="#FFF" />
-                        </Link>
-                    </Popup>
+                {orphanages.map(orphanage => {
+                    return(
+                        <Marker
+                            icon = {mapIcon}
+                            position={[orphanage.latitude,orphanage.longitude]}
+                            key={orphanage.id}
+                        >
+                            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                                {orphanage.name}
+                                <Link to={`/orphanages/${orphanage.id}`}>
+                                    <FiArrowRight size={20} color="#FFF" />
+                                </Link>
+                            </Popup>
 
-                </Marker>
+                        </Marker>
+                    )
+                })}
                
             </Map>
 
